b723cbd8f6d892b8e3fdaf3f9c6ba7e833d0b5f5
Leianny Poiani <leianny.poiani@gmail.com>
2026-03-31 15:27:05 -0300

    Initial commit

---
+++ src/main/java/com/project/user_country_service/UserCountryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.user_country_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCountryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCountryServiceApplication.class, args);
+	}
+
+}
